timfox/sturmgeist

commit 5aae36f2b81cbf40ec0c6c3a1877d7fa9be29e86

mod: fix revert "game: fix team class HP bonus at first spawn, refs #875 #738"
This reverts commit ff61d2bf62d7577a4b8c44ee3855b29f936f481d

src/game/g_active.c
@@ -858,32 +858,35 @@ void ClientTimerActions(gentity_t *ent, int msec)
 		client->timeResidual -= 1000;
 
 		// regenerate
-		if (client->sess.playerType == PC_MEDIC)
+		if (ent->health < client->ps.stats[STAT_MAX_HEALTH])
 		{
-			if (ent->health < client->ps.stats[STAT_MAX_HEALTH])
+			// medic only
+			if (client->sess.playerType == PC_MEDIC)
 			{
 				ent->health += 3;
-				if (ent->health > client->ps.stats[STAT_MAX_HEALTH] * 1.1)
+				if (ent->health > client->ps.stats[STAT_MAX_HEALTH] / 1.11)
 				{
-					ent->health = client->ps.stats[STAT_MAX_HEALTH] * 1.1;
+					ent->health += 2;
+
+					if (ent->health > client->ps.stats[STAT_MAX_HEALTH])
+					{
+						ent->health = client->ps.stats[STAT_MAX_HEALTH];
+					}
+				}
+				else
+				{
+					ent->health += 3;
+					if (ent->health > client->ps.stats[STAT_MAX_HEALTH] / 1.1)
+					{
+						ent->health = client->ps.stats[STAT_MAX_HEALTH] / 1.1;
+					}
 				}
 			}
-			else if (ent->health < client->ps.stats[STAT_MAX_HEALTH] * 1.12)
-			{
-				ent->health += 2;
-				if (ent->health > client->ps.stats[STAT_MAX_HEALTH] * 1.12)
-				{
-					ent->health = client->ps.stats[STAT_MAX_HEALTH] * 1.12;
-				}
-			}
+
 		}
-		else
+		else if (ent->health > client->ps.stats[STAT_MAX_HEALTH])               // count down health when over max
 		{
-			// count down health when over max
-			if (ent->health > client->ps.stats[STAT_MAX_HEALTH])
-			{
-				ent->health--;
-			}
+			ent->health--;
 		}
 	}
 }
@@ -2224,7 +2227,27 @@ void ClientEndFrame(gentity_t *ent)
 	// apply all the damage taken this frame
 	P_DamageFeedback(ent);
 
-	ent->client->ps.stats[STAT_HEALTH] = ent->health;
+	// increases stats[STAT_MAX_HEALTH] based on # of medics in game
+	// AddMedicTeamBonus() now adds medic team bonus and stores in ps.stats[STAT_MAX_HEALTH].
+	AddMedicTeamBonus(ent->client);
+
+	// all players are init in game, we can set properly starting health
+	if (level.startTime == level.time - (GAME_INIT_FRAMES * FRAMETIME))
+	{
+		if (ent->client->sess.skill[SK_BATTLE_SENSE] >= 3)
+		{
+			// We get some extra max health, but don't spawn with that much
+			ent->health = ent->client->ps.stats[STAT_HEALTH] = ent->client->ps.stats[STAT_MAX_HEALTH] - 15;
+		}
+		else
+		{
+			ent->health = ent->client->ps.stats[STAT_HEALTH] = ent->client->ps.stats[STAT_MAX_HEALTH];
+		}
+	}
+	else
+	{
+		ent->client->ps.stats[STAT_HEALTH] = ent->health;
+	}
 
 	G_SetClientSound(ent);
 

src/game/g_client.c
@@ -1333,6 +1333,11 @@ void AddMedicTeamBonus(gclient_t *client)
 		client->pers.maxHealth += 15;
 	}
 
+	if (client->sess.playerType == PC_MEDIC)
+	{
+		client->pers.maxHealth *= 1.12;
+	}
+
 	client->ps.stats[STAT_MAX_HEALTH] = client->pers.maxHealth;
 }
 

src/cgame/cg_draw_hud.c
@@ -630,8 +630,8 @@ void CG_ReadHudScripts(void)
 // HUD DRAWING FUNCTIONS BELLOW
 
 vec4_t HUD_Background = { 0.16f, 0.2f, 0.17f, 0.8f };
-vec4_t HUD_Border     = { 0.5f, 0.5f, 0.5f, 0.5f };
-vec4_t HUD_Text       = { 0.6f, 0.6f, 0.6f, 1.0f };
+vec4_t HUD_Border = { 0.5f, 0.5f, 0.5f, 0.5f };
+vec4_t HUD_Text = { 0.6f, 0.6f, 0.6f, 1.0f };
 
 /**
  * @brief CG_DrawPicShadowed
@@ -827,14 +827,7 @@ static void CG_DrawPlayerHealthBar(rectDef_t *rect)
 	CG_ColorForHealth(colour);
 	colour[3] = 0.5f;
 
-	if (cgs.clientinfo[cg.snap->ps.clientNum].cls == PC_MEDIC)
-	{
-		frac = cg.snap->ps.stats[STAT_HEALTH] / ((float) cg.snap->ps.stats[STAT_MAX_HEALTH] * 1.12f);
-	}
-	else
-	{
-		frac = cg.snap->ps.stats[STAT_HEALTH] / (float) cg.snap->ps.stats[STAT_MAX_HEALTH];
-	}
+	frac = cg.snap->ps.stats[STAT_HEALTH] / (float) cg.snap->ps.stats[STAT_MAX_HEALTH];
 
 	CG_FilledBar(rect->x, rect->y + (rect->h * 0.1f), rect->w, rect->h * 0.84f, colour, NULL, bgcolor, frac, flags);
 
@@ -2578,7 +2571,8 @@ void CG_AddLagometerSnapshotInfo(snapshot_t *snap)
 	}
 
 	cgs.sampledStat.avg = cgs.sampledStat.samplesTotalElpased > 0
-	                      ? (int) (cgs.sampledStat.count / (cgs.sampledStat.samplesTotalElpased / 1000.0f) + 0.5f) : 0;
+	                      ? (int) (cgs.sampledStat.count / (cgs.sampledStat.samplesTotalElpased / 1000.0f) + 0.5f)
+						  : 0;
 }
 
 /**

src/game/bg_misc.c
@@ -2676,20 +2676,9 @@ qboolean BG_CanItemBeGrabbed(const entityState_t *ent, const playerState_t *ps, 
 	case IT_AMMO:
 		return qfalse;
 	case IT_HEALTH:
-		if (ps->stats[STAT_PLAYER_CLASS] == PC_MEDIC)
+		if (ps->stats[STAT_HEALTH] >= ps->stats[STAT_MAX_HEALTH])
 		{
-			// medics can go up to 12% extra on max health as they have perm. regen
-			if (ps->stats[STAT_HEALTH] >= (int)(ps->stats[STAT_MAX_HEALTH] * 1.12))
-			{
-				return qfalse;
-			}
-		}
-		else
-		{
-			if (ps->stats[STAT_HEALTH] >= ps->stats[STAT_MAX_HEALTH])
-			{
-				return qfalse;
-			}
+			return qfalse;
 		}
 		return qtrue;
 	case IT_TEAM:     // team items, such as flags

src/game/g_items.c
@@ -584,8 +584,6 @@ int Pickup_Weapon(gentity_t *ent, gentity_t *other)
  */
 int Pickup_Health(gentity_t *ent, gentity_t *other)
 {
-	int max;
-
 	if (ent->parent && ent->parent->client)
 	{
 		other->client->pers.lasthealth_client = ent->parent->s.clientNum;
@@ -598,16 +596,10 @@ int Pickup_Health(gentity_t *ent, gentity_t *other)
 		G_DebugAddSkillPoints(ent->parent, SK_FIRST_AID, 1.f, "health pack picked up");
 	}
 
-	max = other->client->ps.stats[STAT_MAX_HEALTH];
-	if (other->client->sess.playerType == PC_MEDIC)
-	{
-		max *= 1.12f;
-	}
-
 	other->health += ent->item->quantity;
-	if (other->health > max)
+	if (other->health > other->client->ps.stats[STAT_MAX_HEALTH])
 	{
-		other->health = max;
+		other->health = other->client->ps.stats[STAT_MAX_HEALTH];
 	}
 	other->client->ps.stats[STAT_HEALTH] = other->health;
 

src/game/g_local.h
@@ -1606,6 +1606,7 @@ void player_die(gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int 
 void AddKillScore(gentity_t *ent, int score);
 void CalculateRanks(void);
 qboolean SpotWouldTelefrag(gentity_t *spot);
+void AddMedicTeamBonus(gclient_t *client);
 
 void SetWolfSpawnWeapons(gclient_t *client);
 void limbo(gentity_t *ent, qboolean makeCorpse);

src/game/g_trigger.c
@@ -685,20 +685,9 @@ qboolean G_IsAllowedHeal(gentity_t *ent)
 		return qfalse;
 	}
 
-	if (ent->client->sess.playerType == PC_MEDIC)
+	if (ent->client->ps.stats[STAT_HEALTH] >= ent->client->ps.stats[STAT_MAX_HEALTH])
 	{
-		// medics can go up to 12% extra on max health
-		if (ent->client->ps.stats[STAT_HEALTH] >= (int)(ent->client->ps.stats[STAT_MAX_HEALTH] * 1.12))
-		{
-			return qfalse;
-		}
-	}
-	else
-	{
-		if (ent->client->ps.stats[STAT_HEALTH] >= ent->client->ps.stats[STAT_MAX_HEALTH])
-		{
-			return qfalse;
-		}
+		return qfalse;
 	}
 
 	return qtrue;
@@ -761,14 +750,7 @@ void heal_touch(gentity_t *self, gentity_t *other, trace_t *trace)
 
 	for (i = 0; i < clientcount; i++)
 	{
-		if (touchClients[i]->client->sess.playerType == PC_MEDIC)
-		{
-			healvalue = MIN((int)(touchClients[i]->client->ps.stats[STAT_MAX_HEALTH] * 1.12) - touchClients[i]->health, self->damage);
-		}
-		else
-		{
-			healvalue = MIN(touchClients[i]->client->ps.stats[STAT_MAX_HEALTH] - touchClients[i]->health, self->damage);
-		}
+		healvalue = MIN(touchClients[i]->client->ps.stats[STAT_MAX_HEALTH] - touchClients[i]->health, self->damage);
 
 		if (self->health != -9999)
 		{

commit a55825e94ad753bbde68c86c721af1a6bc979320

game: fix medics spawn at full health, refs #1555 #738
Regression from commit:5aae36f2b81cbf40ec0c6c3a1877d7fa9be29e86

src/game/g_active.c
@@ -2231,6 +2231,11 @@ void ClientEndFrame(gentity_t *ent)
 		{
 			ent->health = ent->client->ps.stats[STAT_HEALTH] = ent->client->ps.stats[STAT_MAX_HEALTH];
 		}
+
+		if (ent->client->sess.playerType == PC_MEDIC)
+		{
+			ent->health = ent->client->ps.stats[STAT_HEALTH] /= 1.12;
+		}
 	}
 	else
 	{

src/game/g_client.c
@@ -642,7 +642,7 @@ void CopyToBodyQue(gentity_t *ent)
 	BODY_TEAM(body)      = ent->client->sess.sessionTeam;
 	BODY_CLASS(body)     = ent->client->sess.playerType;
 	BODY_CHARACTER(body) = ent->client->pers.characterIndex;
-	BODY_VALUE(body)     = 0;
+	BODY_VALUE(body) = 0;
 
 	//if ( ent->client->ps.eFlags & EF_PANTSED ){
 	//	body->s.time2 =	1;
@@ -3041,6 +3041,11 @@ void ClientSpawn(gentity_t *ent, qboolean revived, qboolean teamChange, qboolean
 	else
 	{
 		ent->health = client->ps.stats[STAT_HEALTH] = client->ps.stats[STAT_MAX_HEALTH];
+	}
+
+	if (ent->client->sess.playerType == PC_MEDIC)
+	{
+		ent->health = ent->client->ps.stats[STAT_HEALTH] /= 1.12;
 	}
 
 	G_SetOrigin(ent, spawn_origin);