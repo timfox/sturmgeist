timfox/sturmgeist

commit d90d42bac5e9ab0ad2236ef6fa817c7fb0bfd107

etmain: Minor dyn gunfov tweaks (#2932)
for pistols, pineapple, pliers, ammopack, dynamite & landmines

src/cgame/cg_weapons.c
@@ -72,6 +72,53 @@ static char *weapAnimNumberStr[] =
 
 static vec3_t forward, right, up;
 
+static void CG_Translate(refEntity_t *ref, double trnsl1, double trnsl2, double trnsl3)
+{
+	static vec3_t angles;
+	static vec3_t vec1, vec2, vec3;
+
+	// translate
+	AngleVectors(angles, vec1, vec2, vec3);
+
+	VectorMA(ref->origin, trnsl1, vec1, ref->origin);
+	VectorMA(ref->origin, trnsl2, vec2, ref->origin);
+	VectorMA(ref->origin, trnsl3, vec3, ref->origin);
+}
+
+static void CG_Transform(refEntity_t *ref, double scale, double trnsl1, double trnsl2, double trnsl3, double yaw, double roll, double pitch)
+{
+	static vec3_t angles;
+	static vec3_t vec1, vec2, vec3;
+
+	// scale
+	if (scale != 0.0 || scale != 1.0)
+	{
+		VectorScale(ref->axis[0], scale, ref->axis[0]);
+		VectorScale(ref->axis[1], scale, ref->axis[1]);
+		VectorScale(ref->axis[2], scale, ref->axis[2]);
+	}
+
+	// rotate
+	if (yaw != 0.0 || roll != 0.0 || pitch != 0.0)
+	{
+		AxisToAngles(ref->axis, angles);
+		angles[YAW]   += yaw;
+		angles[ROLL]  += roll;
+		angles[PITCH] += pitch;
+		AnglesToAxis(angles, ref->axis);
+	}
+
+	// translate
+	if (trnsl1 != 0.0 || trnsl2 != 0.0 || trnsl3 != 0.0)
+	{
+		AngleVectors(angles, vec1, vec2, vec3);
+
+		VectorMA(ref->origin, trnsl1, vec1, ref->origin);
+		VectorMA(ref->origin, trnsl2, vec2, ref->origin);
+		VectorMA(ref->origin, trnsl3, vec3, ref->origin);
+	}
+}
+
 /**
  * @brief CG_StartWeaponAnim
  * @param[in] anim
@@ -1874,15 +1921,17 @@ void CG_AddPlayerWeapon(refEntity_t *parent, playerState_t *ps, centity_t *cent)
 					angles[YAW] += 1.5;
 					AnglesToAxis(angles, barrel.axis);
 				}
+				// reposition dynamite relative to hand (it pierced the hand by default)
+				else if (weaponNum == WP_DYNAMITE && i == 0)
+				{
+					CG_Transform(&barrel, 0.9,
+					             0.6, 0.5, 1.4,
+					             13.0, 11.0, 18.0);
+				}
 				// reposition pineapple relative to hand (it hovered by default)
 				else if (weaponNum == WP_GRENADE_PINEAPPLE && i == 0)
 				{
-						AxisToAngles(barrel.axis, angles);
-						AngleVectors(angles, forward, up, left);
-
-						VectorMA(barrel.origin, -0.5f, /* actually backwards */ forward, barrel.origin);
-						VectorMA(barrel.origin, -0.4f, left, barrel.origin);
-						VectorMA(barrel.origin, 0.9f, up, barrel.origin);
+					CG_Translate(&barrel, -0.5, -0.4, 0.9);
 				}
 
 				drawpart = CG_GetPartFramesFromWeap(cent, &barrel, parent, i, weapon);

etmain/weapons/ammopack.weap
@@ -51,8 +51,8 @@ weaponDef
 			model			"models/multiplayer/ammopack/v_ammopack.md3"
 			//flashModel	""
 
-			dynFov90 -3.0 3.0 0.0
-			dynFov120 3.0 -9.5 -1.0
+			dynFov90  -4.0 9.0 0.0
+			dynFov120 1.0 -5.0 -2.0
 		}
 
 		thirdPerson {

etmain/weapons/colt.weap
@@ -74,8 +74,8 @@ weaponDef
 			model			"models/weapons2/colt/v_colt.mdc"
 			flashModel		"models/weapons2/colt/v_colt_flash.mdc"
 
-			dynFov90 -2.0 6.0 2.2
-			dynFov120 3.0 -3.5 2.2
+			dynFov90 -2.0 6.0 1.5
+			dynFov120 3.0 -3.5 1.5
 
 			// weapon building
 			// the parts are numbered because there are some hardcoded references for some weapons to certain parts

etmain/weapons/dynamite.weap
@@ -70,8 +70,8 @@ weaponDef
 			model		"models/weapons2/dynamite/v_dynamite.md3"
 			//flashModel	""
 
-			dynFov90 -1.0 1.0 0.0
-			dynFov120 5.0 -7.0 0.0
+			dynFov90  2.5 1.0 0.0
+			dynFov120 7.0 -7.0 0.0
 
 			// weapon building
 			// the parts are numbered because there are some hardcoded references for some weapons to certain parts

etmain/weapons/landmine.weap
@@ -72,8 +72,8 @@ weaponDef
 			model			"models/multiplayer/landmine/v_landmine.md3"
 			//flashModel	""
 
-			dynFov90 -4.0 6.0 0.0
-			dynFov120 -2.0 0.0 0.0
+			dynFov90  -5.0 8.0 0.0
+			dynFov120 -4.0 2.0 0.0
 		}
 
 		thirdPerson {

etmain/weapons/luger.weap
@@ -74,8 +74,8 @@ weaponDef
 			model			"models/weapons2/luger/v_luger.mdc"
 			flashModel		"models/weapons2/luger/v_luger_flash.mdc"
 
-			dynFov90 0.0 6.0 2.0
-			dynFov120 5.0 -3.0 2.0
+			dynFov90 0.0 6.0 1.5
+			dynFov120 5.0 -3.0 1.5
 
 			// weapon building
 			// the parts are numbered because there are some hardcoded references for some weapons to certain parts

etmain/weapons/pineapple.weap
@@ -78,8 +78,8 @@ weaponDef
 			model			"models/weapons2/grenade/v_pineapple.md3"
 			//flashModel		""
 
-			dynFov90 0.3 3.0 0.0
-			dynFov120 7.0 -7.5 0.0
+			dynFov90 -2.0 3.0 0.0
+			dynFov120 5.0 -7.5 0.0
 
 			// weapon building
 			// the parts are numbered because there are some hardcoded references for some weapons to certain parts

etmain/weapons/pliers.weap
@@ -53,8 +53,8 @@ weaponDef
 			alliedskin	"models/multiplayer/pliers/pliers_allied.skin"
 			//flashModel	""
 
-			dynFov90 -3.5 2.0 0.0
-			dynFov120 -7.8 0.0 6.0
+			dynFov90  -3.5 4.0 0.0
+			dynFov120 -5.5 0.0 6.0
 		}
 
 		thirdPerson {

commit ca2426c8a2c83be35fd3db87ea59fd6097cf9457

cgame: Fix grenade hand reposition regression (#2953)
'CG_Translate' was missing it's own 'AxisToAngles' call.

Similarly harden 'CG_Transform' for the same potential case if rotation
wasn't used.

Regression caused by d90d42bac5e9ab0ad2236ef6fa817c7fb0bfd107

src/cgame/cg_weapons.c
@@ -78,6 +78,7 @@ static void CG_Translate(refEntity_t *ref, double trnsl1, double trnsl2, double 
 	static vec3_t vec1, vec2, vec3;
 
 	// translate
+	AxisToAngles(ref->axis, angles);
 	AngleVectors(angles, vec1, vec2, vec3);
 
 	VectorMA(ref->origin, trnsl1, vec1, ref->origin);
@@ -98,24 +99,29 @@ static void CG_Transform(refEntity_t *ref, double scale, double trnsl1, double t
 		VectorScale(ref->axis[2], scale, ref->axis[2]);
 	}
 
-	// rotate
-	if (yaw != 0.0 || roll != 0.0 || pitch != 0.0)
+	if ((yaw != 0.0 || roll != 0.0 || pitch != 0.0)
+	    || (trnsl1 != 0.0 || trnsl2 != 0.0 || trnsl3 != 0.0))
 	{
 		AxisToAngles(ref->axis, angles);
-		angles[YAW]   += yaw;
-		angles[ROLL]  += roll;
-		angles[PITCH] += pitch;
-		AnglesToAxis(angles, ref->axis);
-	}
 
-	// translate
-	if (trnsl1 != 0.0 || trnsl2 != 0.0 || trnsl3 != 0.0)
-	{
-		AngleVectors(angles, vec1, vec2, vec3);
+		// rotate
+		if (yaw != 0.0 || roll != 0.0 || pitch != 0.0)
+		{
+			angles[YAW]   += yaw;
+			angles[ROLL]  += roll;
+			angles[PITCH] += pitch;
+			AnglesToAxis(angles, ref->axis);
+		}
 
-		VectorMA(ref->origin, trnsl1, vec1, ref->origin);
-		VectorMA(ref->origin, trnsl2, vec2, ref->origin);
-		VectorMA(ref->origin, trnsl3, vec3, ref->origin);
+		// translate
+		if (trnsl1 != 0.0 || trnsl2 != 0.0 || trnsl3 != 0.0)
+		{
+			AngleVectors(angles, vec1, vec2, vec3);
+
+			VectorMA(ref->origin, trnsl1, vec1, ref->origin);
+			VectorMA(ref->origin, trnsl2, vec2, ref->origin);
+			VectorMA(ref->origin, trnsl3, vec3, ref->origin);
+		}
 	}
 }
 
@@ -1827,7 +1833,8 @@ void CG_AddPlayerWeapon(refEntity_t *parent, playerState_t *ps, centity_t *cent)
 			// reappear on the bottom right at the last parts of the animation
 			else if (weaponNum == WP_M7)
 			{
-				if (i == 2 /* right hand */ && cg.predictedPlayerEntity.pe.weap.frame > 130 && cg.predictedPlayerEntity.pe.weap.frame < 136) {
+				if (i == 2 /* right hand */ && cg.predictedPlayerEntity.pe.weap.frame > 130 && cg.predictedPlayerEntity.pe.weap.frame < 136)
+				{
 					continue;
 				}
 			}
@@ -1940,7 +1947,7 @@ void CG_AddPlayerWeapon(refEntity_t *parent, playerState_t *ps, centity_t *cent)
 				// reposition pineapple relative to hand (it hovered by default)
 				else if (weaponNum == WP_GRENADE_PINEAPPLE && i == 0)
 				{
-					CG_Translate(&barrel, -0.5, -0.4, 0.9);
+					CG_Translate(&barrel, -0.5, 0.9, -0.4);
 				}
 
 				drawpart = CG_GetPartFramesFromWeap(cent, &barrel, parent, i, weapon);