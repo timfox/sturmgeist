timfox/sturmgeist

commit 4e1b9f8e542bca74cf21b0f3452dc316a278b417

game: prevent dereference before null check

src/game/g_etbot_interface.cpp
@@ -6142,7 +6142,6 @@ const char *_GetEntityName(gentity_t *_ent)
 	//		return _ent->message;
 	//}
 	static char newentname[256];
-	char *name;
 
 	newentname[0] = '\0';
 
@@ -6179,20 +6178,18 @@ const char *_GetEntityName(gentity_t *_ent)
 			Q_strncpyz(newentname, _ent->message, sizeof(newentname));
 		}
 
-		name = newentname;
+		Q_CleanStr(newentname);
 
-		Q_CleanStr(name);
-		if (name)
+		if (*newentname)
 		{
 			char undschar[] = { '-', (char)NULL };
 			char skipchar[] = { '[', ']', '#', '!', '*', '`',
 				                '^', '&', '<', '>', '+', '=','|',  '\'', '%',
 				                '.', ':', '/', '(', ')', (char)NULL };
 			char *curchar = nullptr;
-			char *tmp;
-			char *tmpdst;
-			tmp = name;
-			tmpdst = name;
+			char *name = newentname;
+			char *tmp = name;
+			char *tmpdst = name;
 
 			while (*tmp)
 			{

commit 7ec00f9bd81f56360cbe3e2f7a2beb0f66614761

game: fix omnibot GetEntityName null value (#3085)
Regression from 4e1b9f8e542bca74cf21b0f3452dc316a278b417

src/game/g_etbot_interface.cpp
@@ -6236,6 +6236,7 @@ const char *_GetEntityName(gentity_t *_ent)
 
 			return name;
 		}
+		return "";
 	}
 	return nullptr;
 }